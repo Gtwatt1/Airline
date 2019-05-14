0ede24b79bcfa371fc7093e866187e946ce003ab
Godwin Olorunshola <godwin@softcom.ng>
2019-05-14 10:12:48 +0100

    Redesign HomeView to remove trip type and passengers

---
+++ BookAirline/SearchAirportVC.swift
@@ -0,0 +1,9 @@
+//
+//  SearchAirportVC.swift
+//  BookAirline
+//
+//  Created by Godwin Olorunshola on 13/05/2019.
+//  Copyright © 2019 Godwin Olorunshola. All rights reserved.
+//
+
+import Foundation
